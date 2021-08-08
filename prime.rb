f03150d7223e3b53009ddf65ec6227b98be65e8f
erdd2218 <github email address>
2021-08-08 11:45:30 -0400

    commit tree 13a0173d2bc0f6a13608bfe6dc098f90bd8ffb87

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(num)
+    #if num < 0 or num == 0 or num == 1
+     # return false
+    #else
+      (2..num-1).to_a.all? do |i|
+        num % i != 0
+      end
+    end
+  end
+    
+
+  def is_prime(num)
+    (2..(num - 1)).each do |n|
+      return false if num % n == 0
+    end
+    true
+  end
